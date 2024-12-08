ff4af1ab9d34f3c068344de0c2fac7566f521953
DIEGO <diego.bme@hotmail.com>
2024-12-08 19:17:58 -0300

    commit com diego

--- rep.c
+++ rep.c
@@ -8,6 +8,7 @@ int main(){
     //continuando nova modificação em branch alternativa
     //alterando com a main
     //alterando com a branch diego
+    //nova alteracao com a b diego
     x+=2;
     return 0;
 }
